4a012e411a435b2054789e7396e2cbe6a890ac64
Uebook <69842121+Uebook@users.noreply.github.com>
2026-01-22 16:43:35 +0530

    Remove unused LinearGradient import from HomeScreen

--- LocalMarketMobile/src/components/HomeScreen.js
+++ LocalMarketMobile/src/components/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import LinearGradient from 'react-native-linear-gradient';
 import Header from './Header';
 import { useThemeColors } from '../hooks/useThemeColors';
 
